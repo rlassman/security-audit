d2f5dc797c64f0a0336d971885f498b3771a6d35
Olivia Boyer <oliviaboyer@wc124-dhcp-235.brynmawr.edu>
2026-04-25 10:59:23 -0400

    evil buyer

--- artdao/test/DAOtestsuite.js
+++ artdao/test/DAOtestsuite.js
@@ -32,6 +32,16 @@ describe("art_DAO", () => {
         [, buyer, artist, holder1, holder2, holder3, holder4] =
             await ethers.getSigners();
     });
+
+
+        async function defaultParams() {
+        return {
+            price: ethers.parseEther("1"),
+            upfront: ethers.parseEther("0.3"),
+            insurance: ethers.parseEther("0.02"),
+            timeframe: 0
+        };
+    }
 //------------------------------------------------------------------------------------------------------
     async function setupNewDAO() {
         const ArtDAO = await ethers.getContractFactory("ArtDAO");
@@ -370,6 +380,7 @@ describe("art_DAO", () => {
 
     //voting process with extensive checks on juror values
     it("has expected juror values throughout voting", async () => {
+
     });
     //checks dispute that ends with no votes
     it("survives dispute with no votes", async () => {
@@ -392,13 +403,63 @@ describe("art_DAO", () => {
         
     });
     //checks close votes
-    it("gives correct results voting", async () => {});
+   // it("gives correct results voting", async () => {});
     //checks if artist can get on jury
     it("doesn't let artist on jury", async () => {});
     //checks if buyer can get on jury
     it("doesn't let buyer on jury", async () => {});
     //check dispute resolution where buyer reverts on receive
-    it("reverts on dispute with evil buyer", async () => {});
+    it("reverts on dispute with evil buyer", async () => {
+        const artDAO = await setupNewDAO();
+        const EvilJuror = await ethers.getContractFactory("EvilBuyer");
+        const evilBuyer = await EvilJuror.deploy();
+        const ERC721Mock = await ethers.getContractFactory("ERC721Mock");
+        const nft = await ERC721Mock.deploy("Test Art", "ART");
+        await nft.mint(artist.address, 1);
+
+        const { price, upfront, insurance, timeframe } = await defaultParams();
+            const ArtCommission = await ethers.getContractFactory("ArtCommission");
+            const commission = await ArtCommission.connect(artist).deploy(
+                evilBuyer.target,
+                artist.address,
+                insurance,
+                price,
+                upfront,
+                timeframe,
+                artDAO.target
+             );
+
+             // regular set up
+             await evilBuyer.confirm(commission.target);
+             
+             // fund
+             const artistFund = insurance / 2n;
+             const buyerFund = insurance / 2n + upfront;
+             await evilBuyer.fund(commission.target, { value: buyerFund });
+             await commission.connect(artist).fund({ value: artistFund });
+
+        await network.provider.send("evm_increaseTime", [7 * 86400]);
+        await network.provider.send("evm_mine"); 
+
+        await commission.connect(artist).raiseDispute(panelSize, votingDuration);
+
+        const disputeId = await commission.disputeId();
+        await artDAO.selectJurors(disputeId, votingDuration);
+
+        const jurors = await artDAO.getJurors(disputeId);
+
+        for (const jurorAddr of jurors) {
+            const juror = await ethers.getSigner(jurorAddr);
+            await artDAO.connect(juror).vote(disputeId, 3);
+        }
+
+        await network.provider.send("evm_increaseTime", [votingDuration + 1]);
+        await network.provider.send("evm_mine");
+
+        await expect(artDAO.resolveDispute(disputeId)).to.be.reverted;
+        //something weird going on here
+        
+    });
     //check dispute resolution where sender revert on receive
     it("reverts on dispute with evil artist", async () => {});
 });
