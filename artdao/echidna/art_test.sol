1250ce09c7525fb1fe4acd960440ac22b2b51e39
Glory Zhang <glorytanzhang@gmail.com>
2026-04-26 08:07:19 -0400

    setup art_test

--- artdao/echidna/art_test.sol
+++ artdao/echidna/art_test.sol
@@ -5,8 +5,20 @@ import 'art_commission.sol';
 contract art_test {
     art_commission level;
 
-    constructor() public payable { 
-        level = art_commission();
+    // deploy fake nft??
+    // using fake addresses for now
+    constructor() ArtCommission(
+        address(0x1), // buyer
+        address(0x2), // artist
+        0.02 ether,   // insuranceAmount
+        1 ether,      // price
+        0.1 ether,    // upfrontPayment
+        7 days,       // timeframe
+        address(0x3)  // DAO
+    ) payable {}
+
+    function test_hacked() public returns (bool) {
+        assert(!(level.owner() == address(this) && address(level).balance == 0));
     }
   
     // full disclosure: I'm writing really random things
@@ -20,11 +32,6 @@ contract art_test {
 
     function test_fallback() external payable {
         address(level).call.value(msg.value)(""); 
-    }
-    
-    function test_hacked() public returns (bool) {
-        //To beat the level you needed to become the owner and withdraw the balance
-        assert(!(level.owner() == address(this) && address(level).balance == 0));
     }
 
     receive() external payable {}
